b45d70edf02ec730af31613c4bd7401b00d99998
ivbelkin <ilya.belkin-trade@yandex.ru>
2020-07-06 23:47:58 +0300

    fix python minimal version

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6',
+    python_requires='>=2.7',
 )
