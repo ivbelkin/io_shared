bfd82be583fd917a07f1fb16ddbfa686d30931cc
ivbelkin <ilya.belkin-trade@yandex.ru>
2020-07-21 11:09:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="io_shared",
-    version="0.0.1",
+    version="0.0.3",
     author="Ilya Belkin",
     author_email="ilya.belkin-trade@yandex.ru",
     description="Read/write safe IPC via shared memory",
